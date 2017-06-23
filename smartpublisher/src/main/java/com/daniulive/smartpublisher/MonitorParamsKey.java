e367111ada92569ea5a987aeefb44cfb68a9e104
120585 <120585@nd.com>
2017-06-23 20:34:30 +0800

    增加MonitorParamsKey的注释

--- smartpublisher/src/main/java/com/daniulive/smartpublisher/MonitorParamsKey.java
+++ smartpublisher/src/main/java/com/daniulive/smartpublisher/MonitorParamsKey.java
@@ -5,7 +5,7 @@ package com.daniulive.smartpublisher;
  */
 
 public class MonitorParamsKey {
-    // 推送类型
+    // 推送类型， 0: screenType 1: camera
     public final static String pushType = "PUSHTYPE";
     // 视频宽
     public final static String camerawidth = "CAMERAWIDTH";
@@ -13,13 +13,13 @@ public class MonitorParamsKey {
     public final static String cameraheight = "CAMERAHEIGHT";
     // 截屏分辨率
     public final static String screenresolution = "SCREENRESOLUTION";
-    // 前后摄像头
+    // 前后摄像头 1：前置摄像头标记 2:后置摄像头标记
     public final static String switchcamera = "SWITCHCAMERA";
     // 推送地址
     public final static String publishurl = "PUBLISHURL";
     // 是否本地保存录制
     public final static String recorder = "RECORDER";
-    // 是否硬解码
+    // 是否硬解码 true: 硬解码 false: 软解码
     public final static String hwencoder = "HWENCODER";
     // 录制音屏地址
     public final static String recoderDir = "RecoderDir";
